be34e28edb45495aeb25d32c3e9e3cf664e55b05
Manoj Kumar Mahato <manoj567770@gmail.com>
2023-09-03 18:47:18 +0530

    Navbar Implementation

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <header className="heading d-flex align-center">
+      <h1 className="heading-1">
+        <a className="link" href="/">
+          TravelO
+        </a>
+      </h1>
+      <nav className="d-flex align-center gap-large">
+        <div className="nav d-flex align-center cursor-pointer">
+          <span className="material-icons-outlined profile-option menu">
+            menu
+          </span>
+          <span className="material-icons-outlined profile-option">
+            person_2
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
+};
