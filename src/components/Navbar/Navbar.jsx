36186e24d93b4d90b5c8bb457ad68c0b1260f3fa
Manoj Kumar Mahato <manoj567770@gmail.com>
2023-09-05 19:28:54 +0530

    Search bar implementation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,14 @@ export const Navbar = () => {
           TravelO
         </a>
       </h1>
+      <div className="form-container d-flex align-center cursor-pointer shadow">
+        <span className="form-option">Any Where</span>
+        <span className="border-right-1px"></span>
+        <span className="form-option">Any Week</span>
+        <span className="border-right-1px"></span>
+        <span className="form-option">Add Guests</span>
+        <span className=" search material-icons-outlined">search</span>
+      </div>
       <nav className="d-flex align-center gap-large">
         <div className="nav d-flex align-center cursor-pointer">
           <span className="material-icons-outlined profile-option menu">
