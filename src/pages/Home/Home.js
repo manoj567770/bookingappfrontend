5fdf298c0239e6b962899d8a157d6dc2a55aa12b
Manoj Kumar Mahato <manoj567770@gmail.com>
2023-09-06 21:02:47 +0530

    Card Implementation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,30 @@
-import { Navbar } from "../../components";
+import { Fragment, useEffect, useState } from "react";
+import { Navbar, HotelCard } from "../../components";
+import "./Home.css";
+import axios from "axios";
+
 export const Home = () => {
-  return <Navbar />;
+  const { hotels, setHotels } = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await axios.get(
+          "https://sore-lime-jellyfish-belt.cyclic.app/api/hotels"
+        );
+        setHotels(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+  return (
+    <Fragment>
+      <Navbar />
+      <main className="main d-flex align-center wrap gap-larger">
+        {hotels &&
+          hotels.map((hotel) => <HotelCard key={hotel._id} hotel={hotel} />)}
+        <HotelCard />
+      </main>
+    </Fragment>
+  );
 };
