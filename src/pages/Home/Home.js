a402dc1134ea17cb612236cbc88545426494ba2b
Manoj Kumar Mahato <manoj567770@gmail.com>
2023-09-07 19:35:42 +0530

    data fetch from api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,11 @@ import "./Home.css";
 import axios from "axios";
 
 export const Home = () => {
-  const { hotels, setHotels } = useState([]);
+  const [hotels, setHotels] = useState([]);
   useEffect(() => {
     (async () => {
       try {
-        const data = await axios.get(
+        const { data } = await axios.get(
           "https://sore-lime-jellyfish-belt.cyclic.app/api/hotels"
         );
         setHotels(data);
@@ -23,7 +23,6 @@ export const Home = () => {
       <main className="main d-flex align-center wrap gap-larger">
         {hotels &&
           hotels.map((hotel) => <HotelCard key={hotel._id} hotel={hotel} />)}
-        <HotelCard />
       </main>
     </Fragment>
   );
